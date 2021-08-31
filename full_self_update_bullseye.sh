44aae9ec14b2cd18604d3e1520d2fda74902d3de
DoMeLe <1812020+djdomi@users.noreply.github.com>
2021-08-31 16:59:22 +0200

    Update full_self_update_bullseye.sh

--- full_self_update_bullseye.sh
+++ full_self_update_bullseye.sh
@@ -13,18 +13,17 @@ ${SUDO} export DEBIAN_FRONTEND=noninteractive
 ${SUDO} export APT_LISTCHANGES_FRONTEND=none
 
 #test of files, that i want to have removed
-${SUDO} test -f /etc/apt/apt.conf.d/20listchanges && tee /etc/apt/apt.conf.d/20listchanges
-${SUDO} rm /etc/apt/apt.conf.d/*proxy* 
+#${SUDO} test -f /etc/apt/apt.conf.d/20listchanges && tee /etc/apt/apt.conf.d/20listchanges
+#${SUDO} rm /etc/apt/apt.conf.d/*proxy* 
 #${SUDO} echo 'Acquire::http::proxy "http://10.0.0.1:9999"; ' | tee /etc/apt/apt.conf.d/99_default_proxy
 
 #disable apt caching behavior due we use apt-cacher-ng and want to save the space
-${SUDO} echo   'Binary::apt::APT::Keep-Downloaded-Packages "false";' 									| tee /etc/apt/apt.conf.d/dont_keep_download_files
-${SUDO} echo   'Dir::Cache "";nDir::Cache::archives "";' 												| tee  /etc/apt/apt.conf.d/00_disable-cache-directories
+${SUDO} echo 'Binary::apt::APT::Keep-Downloaded-Packages "false";' | tee /etc/apt/apt.conf.d/dont_keep_download_files
+${SUDO} echo 'Dir::Cache "";nDir::Cache::archives "";' | tee  /etc/apt/apt.conf.d/00_disable-cache-directories
 
 
 #add default compress options to /etc/logroate.d
 ${SUDO} echo -e compress\\ncompresscmd /usr/bin/xz\\nuncompresscmd /usr/bin/unxz\\ncompressext .xz\\ncompressoptions -T6 -9\\nmaxsize 50M | tee /etc/logrotate.d/0000_compress_all
-${SUDO} apt-get update
 
 #bind 9 source
 
