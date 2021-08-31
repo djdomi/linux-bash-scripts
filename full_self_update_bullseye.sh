d6c4abfbcff43fba7fcc9f825b5a6ee76f71f5c5
DoMeLe <1812020+djdomi@users.noreply.github.com>
2021-08-31 16:56:32 +0200

    Update full_self_update_bullseye.sh

--- full_self_update_bullseye.sh
+++ full_self_update_bullseye.sh
@@ -1,4 +1,4 @@
-
+# /bin/bash -c "$(curl -sL https://raw.githubusercontent.com/djdomi/linux-bash-scripts/master/full_self_update_bullseye.sh?token=AAN2MNBPJAMP452XAYUP2JLBFZBAE)"
 #Check if we need sudo
 if [ "$(whoami)" != "root" ]; then
     SUDO=sudo
@@ -14,7 +14,7 @@ ${SUDO} export APT_LISTCHANGES_FRONTEND=none
 
 #test of files, that i want to have removed
 ${SUDO} test -f /etc/apt/apt.conf.d/20listchanges && tee /etc/apt/apt.conf.d/20listchanges
-${SUDO} rm /etc/apt/apt.conf.d/*proxy*; echo Acquire::http::proxy "http://10.0.0.1:9999"; 				| tee /etc/apt/apt.conf.d/99_default_proxy
+${SUDO} rm /etc/apt/apt.conf.d/*proxy*; echo 'Acquire::http::proxy "http://10.0.0.1:9999"; ' | tee /etc/apt/apt.conf.d/99_default_proxy
 
 #disable apt caching behavior due we use apt-cacher-ng and want to save the space
 ${SUDO} echo   'Binary::apt::APT::Keep-Downloaded-Packages "false";' 									| tee /etc/apt/apt.conf.d/dont_keep_download_files
@@ -34,17 +34,17 @@ ${SUDO} sh -c 'echo "deb https://packages.sury.org/bind/ $(lsb_release -sc) main
 
 
 #Update source.list (make it empty)
-${SUDO} echo 																							| tee /etc/apt/sources.list 
+${SUDO} echo ''																							| tee /etc/apt/sources.list 
 
 #Update sources.list.d
-${SUDO} echo deb     http://deb.debian.org/debian bullseye main contrib non-free						| tee /etc/apt/sources.list.d/main.list
-${SUDO} echo deb-src http://deb.debian.org/debian bullseye main contrib non-free 						| tee -a /etc/apt/sources.list.d/main.list
-${SUDO} echo deb     http://deb.debian.org/debian-security/ bullseye-security main contrib non-free 	| tee -a /etc/apt/sources.list.d/main.list
-${SUDO} echo deb-src http://deb.debian.org/debian-security/ bullseye-security main contrib non-free 	| tee -a /etc/apt/sources.list.d/main.list
-${SUDO} echo deb     http://deb.debian.org/debian bullseye-updates main contrib non-free 				| tee -a /etc/apt/sources.list.d/main.list
-${SUDO} echo deb-src http://deb.debian.org/debian bullseye-updates main contrib non-free 				| tee -a /etc/apt/sources.list.d/main.list
-${SUDO} echo deb     http://deb.debian.org/debian bullseye-backports main contrib non-free 				| tee -a /etc/apt/sources.list.d/main.list
-${SUDO} echo deb-src http://deb.debian.org/debian bullseye-backports main contrib non-free 				| tee -a /etc/apt/sources.list.d/main.list
+${SUDO} echo 'deb     http://deb.debian.org/debian bullseye main contrib non-free'						| tee /etc/apt/sources.list.d/main.list
+${SUDO} echo 'deb-src http://deb.debian.org/debian bullseye main contrib non-free'						| tee -a /etc/apt/sources.list.d/main.list
+${SUDO} echo 'deb     http://deb.debian.org/debian-security/ bullseye-security main contrib non-free' 	| tee -a /etc/apt/sources.list.d/main.list
+${SUDO} echo 'deb-src http://deb.debian.org/debian-security/ bullseye-security main contrib non-free' 	| tee -a /etc/apt/sources.list.d/main.list
+${SUDO} echo 'deb     http://deb.debian.org/debian bullseye-updates main contrib non-free' 				| tee -a /etc/apt/sources.list.d/main.list
+${SUDO} echo 'deb-src http://deb.debian.org/debian bullseye-updates main contrib non-free' 				| tee -a /etc/apt/sources.list.d/main.list
+${SUDO} echo 'deb     http://deb.debian.org/debian bullseye-backports main contrib non-free' 				| tee -a /etc/apt/sources.list.d/main.list
+${SUDO} echo 'deb-src http://deb.debian.org/debian bullseye-backports main contrib non-free' 				| tee -a /etc/apt/sources.list.d/main.list
 
 
 
