010e9f68db92329a5ed38a50efae4d8696250f27
DoMeLe <1812020+djdomi@users.noreply.github.com>
2021-09-01 15:16:45 +0200

    Update full_self_update_bullseye.sh

--- full_self_update_bullseye.sh
+++ full_self_update_bullseye.sh
@@ -18,7 +18,7 @@ dpkg --configure -a --force-confold --force-confdef
 ${SUDO} echo -e USE_DPKG\\nMANDELETE\\nDONTBOTHERNEWLOCALE\\nSHOWFREEDSPACE\\nde\\nde_DE\\nde_DE.UTF-8\\nde_DE@euro\\nen\\nen_US\\nen_US.ISO-8859-15\\nen_US.UTF-8 | tee /etc/locale.nopurge
 # Install pre-requirements
 DEBIAN_FRONTEND=noninteractive 
-${SUDO} apt-get -yq install apt-transport-https lsb-release ca-certificates curl localepurge aria2 software-properties-common -o DPkg::options::="--force-confdef" -o DPkg::options::="--force-confold"
+${SUDO} apt-get -yqq install apt-transport-https lsb-release ca-certificates curl localepurge aria2 software-properties-common -o DPkg::options::="--force-confdef" -o DPkg::options::="--force-confold"
 
 #Export Variables we want to use
 ${SUDO} export DEBIAN_FRONTEND=noninteractive
@@ -27,11 +27,11 @@ apt
 #test of files, that i want to have removed
 ${SUDO} test -f /etc/apt/apt.conf.d/20listchanges && apt -y remove --purge apt-listchanges
 ${SUDO} rm -f /etc/apt/apt.conf.d/*proxy* 
-${SUDO} echo 'Acquire::http::proxy "http://10.0.0.1:9999"; ' | tee /etc/apt/apt.conf.d/99_default_proxy 2&>1 >/dev/null
+${SUDO} echo 'Acquire::http::proxy "http://10.0.0.1:9999"; ' | tee /etc/apt/apt.conf.d/99_default_proxy 2>&1 >/dev/null
 
 #disable apt caching behavior due we use apt-cacher-ng and want to save the space
-${SUDO} echo 'Binary::apt::APT::Keep-Downloaded-Packages "false";' | tee /etc/apt/apt.conf.d/dont_keep_download_files 2&>1 >/dev/null
-${SUDO} echo 'Dir::Cache "";nDir::Cache::archives "";' | tee  /etc/apt/apt.conf.d/00_disable-cache-directories 2&>1 >/dev/null
+${SUDO} echo 'Binary::apt::APT::Keep-Downloaded-Packages "false";' | tee /etc/apt/apt.conf.d/dont_keep_download_files 2>&1 >/dev/null
+${SUDO} echo 'Dir::Cache "";nDir::Cache::archives "";' | tee  /etc/apt/apt.conf.d/00_disable-cache-directories 2>&1 >/dev/null
 
 
 #add default compress options to /etc/logroate.d
@@ -41,8 +41,8 @@ ${SUDO} echo -e compress\\ncompresscmd /usr/bin/xz\\nuncompresscmd /usr/bin/unxz
 rm -f /etc/apt/trusted.gpg.d/bind.gpg /etc/apt/trusted.gpg.d/php.gpg
 ${SUDO} wget -qO /etc/apt/trusted.gpg.d/bind.gpg https://packages.sury.org/bind/apt.gpg 
 ${SUDO} wget -qO /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
-${SUDO} sh -c 'echo deb https://packages.sury.org/php/ bullseye main'   | tee /etc/apt/sources.list.d/bind.list 2&>1 >/dev/null
-${SUDO} sh -c 'echo "deb https://packages.sury.org/bind/ bullseye main' | tee /etc/apt/sources.list.d/bind.list 2&>1 >/dev/null
+${SUDO} echo 'deb https://packages.sury.org/php/ bullseye main'   | tee /etc/apt/sources.list.d/bind.list 2>&1 >/dev/null
+${SUDO} echo 'deb https://packages.sury.org/bind/ bullseye main' | tee /etc/apt/sources.list.d/bind.list 2>&1 >/dev/null
 
 clear
 
@@ -50,28 +50,28 @@ echo adding sources
 
 
 #Update source.list (make it empty)
-${SUDO} echo ''																							| tee /etc/apt/sources.list 2&>1 >/dev/null
+${SUDO} echo ''																							| tee /etc/apt/sources.list 2>&1 >/dev/null
 
 #Update sources.list.d
-${SUDO} echo 'deb     http://deb.debian.org/debian bullseye main contrib non-free'							| tee /etc/apt/sources.list.d/main.list 2&>1 >/dev/null
-${SUDO} echo 'deb-src http://deb.debian.org/debian bullseye main contrib non-free'							| tee -a /etc/apt/sources.list.d/main.list 2&>1 >/dev/null
-${SUDO} echo 'deb     http://deb.debian.org/debian-security/ bullseye-security main contrib non-free' 		| tee -a /etc/apt/sources.list.d/main.list 2&>1 >/dev/null
-${SUDO} echo 'deb-src http://deb.debian.org/debian-security/ bullseye-security main contrib non-free' 		| tee -a /etc/apt/sources.list.d/main.list 2&>1 >/dev/null
-${SUDO} echo 'deb     http://deb.debian.org/debian bullseye-updates main contrib non-free' 					| tee -a /etc/apt/sources.list.d/main.list 2&>1 >/dev/null
-${SUDO} echo 'deb-src http://deb.debian.org/debian bullseye-updates main contrib non-free' 					| tee -a /etc/apt/sources.list.d/main.list 2&>1 >/dev/null
-${SUDO} echo 'deb     http://deb.debian.org/debian bullseye-backports main contrib non-free' 				| tee -a /etc/apt/sources.list.d/main.list 2&>1 >/dev/null
-${SUDO} echo 'deb-src http://deb.debian.org/debian bullseye-backports main contrib non-free' 				| tee -a /etc/apt/sources.list.d/main.list 2&>1 >/dev/null
-${SUDO} echo 'deb http://deb.debian.org/debian bullseye-proposed-updates main contrib non-free'				| tee -a /etc/apt/sources.list.d/main.list 2&>1 >/dev/null
-${SUDO} echo 'deb-src http://deb.debian.org/debian bullseye-proposed-updates main contrib non-free'			| tee -a /etc/apt/sources.list.d/main.list 2&>1 >/dev/null
+${SUDO} echo 'deb     http://deb.debian.org/debian bullseye main contrib non-free'							| tee /etc/apt/sources.list.d/main.list 2>&1 >/dev/null
+${SUDO} echo 'deb-src http://deb.debian.org/debian bullseye main contrib non-free'							| tee -a /etc/apt/sources.list.d/main.list 2>&1 >/dev/null
+${SUDO} echo 'deb     http://deb.debian.org/debian-security/ bullseye-security main contrib non-free' 		| tee -a /etc/apt/sources.list.d/main.list 2>&1 >/dev/null
+${SUDO} echo 'deb-src http://deb.debian.org/debian-security/ bullseye-security main contrib non-free' 		| tee -a /etc/apt/sources.list.d/main.list 2>&1 >/dev/null
+${SUDO} echo 'deb     http://deb.debian.org/debian bullseye-updates main contrib non-free' 					| tee -a /etc/apt/sources.list.d/main.list 2>&1 >/dev/null
+${SUDO} echo 'deb-src http://deb.debian.org/debian bullseye-updates main contrib non-free' 					| tee -a /etc/apt/sources.list.d/main.list 2>&1 >/dev/null
+${SUDO} echo 'deb     http://deb.debian.org/debian bullseye-backports main contrib non-free' 				| tee -a /etc/apt/sources.list.d/main.list 2>&1 >/dev/null
+${SUDO} echo 'deb-src http://deb.debian.org/debian bullseye-backports main contrib non-free' 				| tee -a /etc/apt/sources.list.d/main.list 2>&1 >/dev/null
+${SUDO} echo 'deb http://deb.debian.org/debian bullseye-proposed-updates main contrib non-free'				| tee -a /etc/apt/sources.list.d/main.list 2>&1 >/dev/null
+${SUDO} echo 'deb-src http://deb.debian.org/debian bullseye-proposed-updates main contrib non-free'			| tee -a /etc/apt/sources.list.d/main.list 2>&1 >/dev/null
 
 
 #we re-set the Options we want to use
-${SUDO} echo -e USE_DPKG\\nMANDELETE\\nDONTBOTHERNEWLOCALE\\nSHOWFREEDSPACE\\nde\\nde_DE\\nde_DE.UTF-8\\nde_DE@euro\\nen\\nen_US\\nen_US.ISO-8859-15\\nen_US.UTF-8 | tee /etc/locale.nopurge 2&>1 >/dev/null
+${SUDO} echo -e USE_DPKG\\nMANDELETE\\nDONTBOTHERNEWLOCALE\\nSHOWFREEDSPACE\\nde\\nde_DE\\nde_DE.UTF-8\\nde_DE@euro\\nen\\nen_US\\nen_US.ISO-8859-15\\nen_US.UTF-8 | tee /etc/locale.nopurge 2>&1 >/dev/null
 
 
 
 # start apt stuff
-${SUDO} apt-get update -qy
+${SUDO} apt-get update -qqy
 DEBIAN_FRONTEND=noninteractive 
 ${SUDO} apt-get dist-upgrade -qy -o DPkg::options::="--force-confdef" -o DPkg::options::="--force-confold"
 ${SUDO} apt-get autoremove -qy
